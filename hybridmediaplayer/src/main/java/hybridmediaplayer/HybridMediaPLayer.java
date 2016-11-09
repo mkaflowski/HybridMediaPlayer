6d114b30cefbcd5c408e7efd3ad6d1b45c0f070e
Mateusz <mkaflowski@gmail.com>
2016-11-09 16:17:16 +0100

    fixes

--- hybridmediaplayer/src/main/java/hybridmediaplayer/HybridMediaPLayer.java
+++ hybridmediaplayer/src/main/java/hybridmediaplayer/HybridMediaPLayer.java
@@ -6,7 +6,6 @@ public abstract class HybridMediaPlayer {
     OnPreparedListener onPreparedListener = null;
     OnCompletionListener onCompletionListener = null;
     OnErrorListener onErrorListener = null;
-    int b;
 
     public static HybridMediaPlayer getInstance(Context context) {
         HybridMediaPlayer res;
