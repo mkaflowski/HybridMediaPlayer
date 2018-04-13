0b0a0fbd66d41215c2daa39e62148b1b124a220b
mateuszkaflowski <mkaflowski@gmail.com>
2018-04-13 16:42:07 +0200

    listener fixes

--- hybridmediaplayer/src/main/java/hybridmediaplayer/ExoMediaPlayer.java
+++ hybridmediaplayer/src/main/java/hybridmediaplayer/ExoMediaPlayer.java
@@ -35,7 +35,6 @@ import com.google.android.gms.cast.MediaMetadata;
 import com.google.android.gms.cast.MediaQueueItem;
 import com.google.android.gms.cast.framework.CastContext;
 import com.google.android.gms.common.images.WebImage;
-import com.socks.library.KLog;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -484,8 +483,6 @@ public class ExoMediaPlayer extends HybridMediaPlayer implements CastPlayer.Sess
         @Override
         public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
             super.onPlayerStateChanged(playWhenReady, playbackState);
-
-            KLog.d("onPlayerStateChanged " + playbackState);
 
             if (currentPlayer != player)
                 return;
